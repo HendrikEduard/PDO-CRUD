9e93f7cbe318992b590d498f3b7d9a5f62996b34
HendrikEduard <hekuiper@gmail.com>
2018-08-02 17:06:44 +0200

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -6,8 +6,8 @@ define('SITE_URL','localhost/');
 // DB credentials.
 $host = '127.0.0.1';
 $db   = 'Ghp02FgpuqfbeO';
-$user = 'root';
-$pass = 'password';
+$user = '_anything_other_than_root';
+$pass = '_anything_other_than_password';
 $charset = 'utf8mb4';
 
 require_once 'functions.php';
